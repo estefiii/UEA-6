5a930b2cccadc20f2d80125dac3b532384f7e1c6
Estefi <2233072978@correo.ler.uam.mx>
2025-07-21 22:49:07 -0600

    tarea

--- prueba/script.js
+++
@@ -1,4 +0,0 @@
-// Aquí puedes agregar interactividad si quieres después 💅
-// Por ejemplo: animaciones, botones interactivos, etc.
-
-console.log("✨ 'Solo es un consejo' cargado ✨");
--- script.js
+++ script.js
@@ -1,1 +1,4 @@
-console.log("Hola bbs");
+// Aquí puedes agregar interactividad si quieres después 💅
+// Por ejemplo: animaciones, botones interactivos, etc.
+
+console.log("✨ 'Solo es un consejo' cargado ✨");
